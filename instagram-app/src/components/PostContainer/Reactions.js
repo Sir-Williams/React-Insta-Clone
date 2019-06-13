cd5303bd5cedf756cb4274f042bf12f86ba49fae
Sir-Williams <sirjohnwilliams@live.com>
2019-06-13 13:28:33 -0700

    completed reactions.js

--- instagram-app/src/components/PostContainer/Reactions.js
+++ instagram-app/src/components/PostContainer/Reactions.js
@@ -3,9 +3,17 @@ import React from 'react';
 const Reactions = props => {
     return [
     <div className="reactions" key="likes-container" onClick={props.plusLike}>
-        <div>
-            <i className="fav" />
+        <div className="reaction-container">
+            <i className="far fa-fav" />
         </div>
+        <div className="reaction-container">
+            <i className="far fa-comment" />
+        </div>
+    </div>,
+    <div className="like-section" key="likes-container">
+        <div className="like-section-container">{props.likes}</div>
     </div>
     ];
 };
+
+export default Reactions;
